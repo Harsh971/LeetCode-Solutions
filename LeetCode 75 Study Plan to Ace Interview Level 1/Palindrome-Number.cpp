a25ce78ed86e997ad27ed946ea3dd3f45d1b4007
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-08-29 11:32:12 +0530

    CPP Solution

---
+++ LeetCode 75 Study Plan to Ace Interview Level 1/Palindrome-Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        else
+        {
+            long reverse=0;
+            int temp=x;
+            while(temp)
+            {
+                reverse = reverse*10 + temp%10;
+                temp = temp / 10;
+            }
+            return reverse==x;
+        }
+    }
+};
