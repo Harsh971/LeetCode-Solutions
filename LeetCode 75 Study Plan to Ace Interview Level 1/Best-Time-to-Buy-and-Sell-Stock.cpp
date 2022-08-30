3ae7eb4ec09ccaf011c183abcda514ecf6be44d3
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-08-30 09:29:08 +0530

    CPP Solution

---
+++ LeetCode 75 Study Plan to Ace Interview Level 1/Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int sell = 0;
+        int hold = INT_MIN;
+
+        for (const int price : prices) 
+        {
+            sell = max(sell, hold + price);
+            hold = max(hold, -price);
+        }
+
+    return sell;
+    }
+};
